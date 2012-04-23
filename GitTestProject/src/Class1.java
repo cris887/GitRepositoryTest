7e535c42ea56a6cf601086161fdb485c19cf08f7
cris887 <cris_887@yahoo.com>
2012-04-23 16:39:13 +0300

    * update Class1

--- GitTestProject/src/Class1.java
+++ GitTestProject/src/Class1.java
@@ -8,29 +8,5 @@ public class Class1 {
 	
 	private String str3;
 	
-	public boolean isExists() {
-		return exists;
-	}
-
-	public String getStr() {
-		return str;
-	}
-
-	public void setStr(String str) {
-		this.str = str;
-	}
-
-	public void setExists(boolean exists) {
-		this.exists = exists;
-	}
-
-	public String getStr2() {
-		return str2;
-	}
-
-	public void setStr2(String str2) {
-		this.str2 = str2;
-	}
-
 	
 }
